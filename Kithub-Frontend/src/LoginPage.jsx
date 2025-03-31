ff1b44f3e962ef39fe5a77fefc8172160e93da37
diego <diego.medina@my.utsa.edu>
2025-03-31 13:23:32 -0500

    Added Login Page Component
    😭 :3 sah haaa

---
+++ Kithub-Frontend/src/LoginPage.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import './loginPage.css'
+function loginPage(){
+    const [userName,setuserName] = useState('');
+    const [password,setPassword] = useState('');
+
+    function handleUsernameChange(event){
+        setuserName(event.target.value);
+    };
+
+    function handlePasswordChange(event){
+        setPassword(event.target.value);
+    };
+
+    function handleSubmit(event){
+        event.preventDefault();
+        console.log('Username:', userName);
+        console.log('Password:', password);
+
+    };
+
+    return (
+        <div className ="login-form">
+            <h2>Welcome to KitHub</h2>
+            <form onSubmit = {handleSubmit}>
+                <div className="cred-box">
+                <input
+                    type="text"
+                    id="username"
+                    placeholder='Enter Username...'
+                    onChange = {handleUsernameChange}
+                    value={userName}
+                    />
+                
+                <input
+                    type="password"
+                    id="password"
+                    placeholder='Enter Password...'
+                    onChange = {handlePasswordChange}
+                    value={password}
+                    /> 
+                  </div>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+}
+export default loginPage;
