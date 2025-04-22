304a800b6473588d04bb670a8cb14abc2f0302e7
Jibril-Pa <109011292+Jibril-Pa@users.noreply.github.com>
2025-04-22 15:01:12 -0500

    Stupid-commit

--- Kithub-Frontend/src/App.jsx
+++ Kithub-Frontend/src/App.jsx
@@ -1,18 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from './LoginPage';
 import Home from './Home';
-<<<<<<< HEAD
 import Register from './Register';
-=======
->>>>>>> Diego-
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
-<<<<<<< HEAD
       <Route path="/register" element={<Register />} />
-=======
->>>>>>> Diego-
       <Route path="/home" element={<Home />} />
     </Routes>
   );
